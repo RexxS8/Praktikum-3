129a4fa45131536c001160730e4fc01f9c8ed512
RexxS8 <andri4martin@gmail.com>
2022-10-02 23:45:45 +0700

    Create Praktikum 3 Latihan 1 Andri Martin.py

---
+++ Praktikum 3 Latihan 1 Andri Martin.py
@@ -0,0 +1,14 @@
+#Andri Martin 064002200010
+# Program Menentukan Jenis Segitiga
+print("Program Menentukan Jenis Segitiga")
+a = input("Masukan Sisi A Segitiga: ")
+b = input("Masukan Sisi B Segitiga: ")
+c = input("Masukan Sisi C Segitiga: ")
+#Variabel untuk segitiga sama sisi
+if a == b == c:
+        print("segitiga Sama Sisi")
+elif a == b or a == c or b == c: 
+        print("Segitiga Sama Kaki ")
+else:
+        print("segitiga Sembarang")
+print("064002200010 - Andri Martin")
